97ee10a2ef7847f3dd7afe50d9ee68fc394f3b12
Barasa <timbaros87@gmail.com>
2024-08-17 16:00:24 +0300

    Getting Remote Data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,18 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self):          
+        
+        # Send a GET request to the URL and return the content
+        response = requests.get(self.url)
+        return response.content
+    
 
     def load_json(self):
-        pass
+        # Load the JSON content into a Python dictionary
+        response_content = self.get_response_body()
+
+        # Load the JSON content into a Python dictionary
+        data = json.loads(response_content)
+        
+        return data
